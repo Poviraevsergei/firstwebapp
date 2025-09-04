5bb2c95cc409189a702e1306e4a814657b478912
poviraevsergei <Siarhey.Pavirayeu@ventionteams.com>
2025-09-04 20:34:01 +0300

    xml servlet

---
+++ src/main/java/com/tms/MyFirstServlet.java
@@ -0,0 +1,19 @@
+package com.tms;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class MyFirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        Writer writer = resp.getWriter();
+        writer.append("<html> My first servlet :)</html>");
+        writer.close();
+    }
+}
